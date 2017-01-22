f783b1b9a9e56d629b6892486b6bcc687ec6d5c6
mf-tipton <matthewt@workamajig.com>
2017-01-22 18:35:12 -0500

    Database connectivity with LINQ

--- web/Default.aspx.cs
+++ web/Default.aspx.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using web.modules;
 
 namespace web
 {
     public partial class Default : System.Web.UI.Page
     {
-        protected string test;
+        protected string test = "";
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            test = "Hello World!";
+            modules.linq_poc q = new modules.linq_poc();
+
+            q.processStringQuery();
+            linq_poc db = new linq_poc();
+
+            foreach(var p in db.getPeople()){
+                if(test.Length > 0) test += "<br />";
+                test += p.FirstName;
+            }
+            test += "<br />Hello World!";
         }
     }
 }
---
+++ web/modules/linq_poc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+
+namespace web.modules
+{
+    public class linq_poc
+    {
+        public Array result;
+
+        private TVL_DBDataContext db = new TVL_DBDataContext();
+
+        static void Main()
+        {
+            
+        }
+
+        public void processStringQuery()
+        {
+            string[] words = { "hello", "wonderful", "LINQ", "beautiful", "world" };
+            ArrayList temp = new ArrayList();
+
+            var shortWords = from word in words
+                             where word.Length <= 5
+                             select word;
+
+            foreach (var word in shortWords)
+            {
+                temp.Add(word);
+            }
+
+            result = temp.ToArray();
+        }
+
+        public IQueryable<Person> getPeople()
+        {
+            IQueryable<Person> people = from p in db.Persons
+                                         select p;
+
+            return people;
+        }
+    }
+}
